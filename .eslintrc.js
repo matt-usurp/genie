9f5c76561779d110cb672c80bec2ca4b808394a3
Matthew Rose <matt.usurp@gmail.com>
2021-01-17 13:56:53 +0000

    chore: initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  root: true,
+
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+
+  env: {
+    'jest/globals': true,
+  },
+
+  rules: {
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': ['error'],
+
+    'no-throw-literal': 'off',
+    '@typescript-eslint/no-throw-literal': ['error'],
+
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+
+    ///
+    ///
+    ///
+
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': ['error'],
+    '@typescript-eslint/prefer-readonly': ['error'],
+    '@typescript-eslint/promise-function-async': ['error'],
+  },
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Remedy
+
+A simplistic service container and parameter store.
+
+## Usage
+
+> The package is in a non-1.0 release state.
+> The public API and types might change or be moved within any minor version change.
+> A change log will be provided where possible to allow more frictionless upgrade.
